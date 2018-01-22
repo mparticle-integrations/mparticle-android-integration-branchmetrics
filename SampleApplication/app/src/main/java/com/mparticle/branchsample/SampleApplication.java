587107a8d2d274da3f86dc84c0210925fae4808a
Sam Dozor <sdozor@mparticle.com>
2018-01-22 17:16:51 -0500

    Update sample app

--- SampleApplication/app/src/main/java/com/mparticle/branchsample/SampleApplication.java
+++ SampleApplication/app/src/main/java/com/mparticle/branchsample/SampleApplication.java
@@ -2,33 +2,73 @@ package com.mparticle.branchsample;
 
 import android.app.Application;
 
-import com.mparticle.DeepLinkError;
-import com.mparticle.DeepLinkListener;
-import com.mparticle.DeepLinkResult;
+import com.mparticle.AttributionError;
+import com.mparticle.AttributionListener;
+import com.mparticle.AttributionResult;
 import com.mparticle.MParticle;
+import com.mparticle.MParticleOptions;
 
-public class SampleApplication extends Application {
+import org.json.JSONObject;
+
+public class SampleApplication extends Application implements AttributionListener {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        MParticle.start(this);
-        MParticle instance = MParticle.getInstance();
+        MParticleOptions options = MParticleOptions.builder(this)
+                .credentials("REPLACE ME", "REPLACE ME")
+                //Set an instance of AttributionListener
+                //this instance will continually be called whenever
+                //there is new attribution/deep linking information.
+                //It is essentially equivalent to the Branch SDK's BranchReferralInitListener class
+                //that is passed to the Branch SDK's initSession() API.
+                .attributionListener(this)
+                .logLevel(MParticle.LogLevel.VERBOSE)
+                .build();
+        MParticle.start(options);
+    }
 
-        instance.setLogLevel(MParticle.LogLevel.DEBUG);
-        instance.setDeepLinkListener(new DeepLinkListener() {
-            @Override
-            public void onResult(DeepLinkResult deepLinkResult) {
-                //TODO
-                //implement logic for deferred deep linking when user has to newly install application
-            }
+    /**
 
-            @Override
-            public void onError(DeepLinkError deepLinkError) {
-                //TODO
-                // implement error handling logic
+      A few typical scenarios where this callback would be invoked:
+
+      (1) Base case:
+        - User does not tap on a link, and then opens the app (either after a fresh install or not)
+         - This block will be invoked with Branch Metrics' response indicating that this user did not tap on a link.
+
+      (2) Deferred deep link:
+         - User without the app installed taps on a link
+          - User is redirected from Branch Metrics to the App Store and installs the app
+          - User opens the app
+         - This block will be invoked with Branch Metrics' response containing the details of the link
+
+      (3) Deep link with app installed:
+         - User with the app already installed taps on a link
+         - Application opens directly to an Activity via a link click, mParticle forwards the launch URI etc to Branch
+         - This callback will be invoked with Branch Metrics' response containing the details of the link
+
+        If the user navigates away from the app without killing it, this callback could be invoked several times:
+        once for the initial launch, and then again each time the user taps on a link to re-open the app.
+     * @param attributionResult
+     **/
+    @Override
+    public void onResult(AttributionResult attributionResult) {
+        //this will be invoked for
+        if (attributionResult.getServiceProviderId() == MParticle.ServiceProviders.BRANCH_METRICS) {
+            JSONObject params = attributionResult.getParameters();
+            // The Branch SDK will return a response for all new session/app opens, even
+            // if the user did not click a branch link.
+            // The parameters supported by the Branch SDK are documented here:
+            // https://github.com/BranchMetrics/android-branch-deep-linking#branch-provided-data-parameters-in-initsession-callback
+            if (params.optBoolean("+clicked_branch_link", false)) {
+                //handle the Branch link click
             }
-        });
+        }
+    }
+
+    @Override
+    public void onError(AttributionError attributionError) {
+        //if the Branch SDK returns an error, it will be surfaced here.
     }
 }
